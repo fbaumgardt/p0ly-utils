9d0ad1b82ae306f2c47e60a293a6512d9c5eb7fd
Frederik Baumgardt <fbaumgardt@posteo.de>
2026-05-24 15:09:10 -0400

    Add experiment specs

--- src/p0ly_utils/metadata/dmss.py
+++ src/p0ly_utils/metadata/dmss.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import numpy as np, pandas as pd
 
 block_ids = ['Stim/S  3','Stim/S  4']
@@ -40,3 +41,58 @@ def get_metadata(evt,ids,f=None):
             md['RT'].extend([end-begin])
     return pd.DataFrame(md)
 
+=======
+# Event codes for DMSS
+from p0ly_utils.metadata.core import (
+    ExperimentSpec,
+    IntSum,
+    PairedMarkers,
+    RTMeasure,
+)
+from p0ly_utils.metadata.parser import parse_metadata
+
+block_ids = ["Stim/S  3", "Stim/S  4"]
+trial_ids = ["Stim/S  5", "Stim/S  6"]
+probe_id = "Stim/S 57"
+stimulus_id = {"size1": "Stim/S 11", "size2": "Stim/S 25", "size4": "Stim/S 43"}
+response_id = {"all": "Stim/S 60", "correct": "Stim/S 64", "incorrect": "Stim/S 65"}
+feedback_id = {"size1": "Stim/S 72", "size2": "Stim/S 73", "size4": "Stim/S 74"}
+
+timelocks = {
+    "stim": {"size1": "Stim/S 11", "size2": "Stim/S 25", "size4": "Stim/S 43"},
+    "prob": {"all": "Stim/S 57"},
+    "resp": {"correct": "Stim/S 64", "incorrect": "Stim/S 65"},
+    "fdb": {"size1": "Stim/S 72", "size2": "Stim/S 73", "size4": "Stim/S 74"},
+}
+
+intervals = {
+    "stim": (-0.2, 1.2),
+    "prob": (-0.2, 1.2),
+    "fdb": (-0.2, 1.0),
+    "resp": (-1.2, 0.2),
+}
+
+spec = ExperimentSpec(
+    name="dmss",
+    timelocks=timelocks,
+    intervals=intervals,
+    block_strategy=PairedMarkers("Stim/S  3", "Stim/S  4"),
+    trial_strategy=PairedMarkers("Stim/S  5", "Stim/S  6"),
+    columns={
+        "Size": IntSum({"Stim/S 11": 1, "Stim/S 25": 2, "Stim/S 43": 4}),
+        "Correct": IntSum({"Stim/S 64": 1}),
+    },
+    rt_defs=[
+        RTMeasure(
+            "RT",
+            start=["Stim/S 57"],
+            end=["Stim/S 60"],
+            nan_if_negative=False,
+        )
+    ],
+)
+
+
+def get_metadata(evt, ids, f=None):
+    return parse_metadata(spec, evt, ids, csv_path=f)
+>>>>>>> Stashed changes
--- src/p0ly_utils/metadata/mgsearch.py
+++ src/p0ly_utils/metadata/mgsearch.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Event codes for Memory Guided Search -- TODO: Add attend red/green code, Confirm correctness of yes/no ids
 import numpy as np, pandas as pd
 
@@ -75,3 +76,75 @@ def get_metadata(evt,ids,f=None):
                 rt = float('nan')
             md['RT'].extend([rt])
     return pd.DataFrame(md)
+=======
+# Event codes for Memory Guided Search
+from p0ly_utils.metadata.core import (
+    CodeLookup,
+    ExperimentSpec,
+    InferFromColumn,
+    IntSum,
+    PairedMarkers,
+    RTMeasure,
+)
+from p0ly_utils.metadata.parser import parse_metadata
+
+trial_ids = ["Stim/S182", "Stim/S183"]
+
+timelocks = {
+    "stim": {"left": "Stim/S 10", "right": "Stim/S 11"},
+    "prob": {"nomatch": "Stim/S110", "match": "Stim/S111"},
+    "resp": {"correct": "Stim/S189", "incorrect": "Stim/S190", "none": "Stim/S191"},
+}
+
+intervals = {
+    "stim": (-0.2, 1.2),
+    "prob": (-0.2, 1.2),
+    "resp": (-1.2, 0.2),
+}
+
+rt_ids = {
+    "start": [timelocks["prob"]["nomatch"], timelocks["prob"]["match"]],
+    "end": [
+        timelocks["resp"]["incorrect"],
+        timelocks["resp"]["correct"],
+        timelocks["resp"]["none"],
+    ],
+}
+
+spec = ExperimentSpec(
+    name="mgsearch",
+    timelocks=timelocks,
+    intervals=intervals,
+    block_strategy=InferFromColumn("Cue_side", end=-1),
+    trial_strategy=PairedMarkers("Stim/S182", "Stim/S183", offset=(1, 0)),
+    columns={
+        "TO1": IntSum({f"Stim/S{201 + i}": i + 1 for i in range(8)}),
+        "TO2": IntSum({f"Stim/S{209 + i}": i + 1 for i in range(8)}),
+        "DO1": IntSum({f"Stim/S{217 + i}": i + 1 for i in range(8)}),
+        "DO2": IntSum({f"Stim/S{225 + i}": i + 1 for i in range(8)}),
+        "NO1": IntSum({f"Stim/S{233 + i}": i + 1 for i in range(8)}),
+        "NO2": IntSum({f"Stim/S{241 + i}": i + 1 for i in range(8)}),
+        "Cue_side": CodeLookup({"left": "Stim/S 10", "right": "Stim/S 11"}),
+        "TT_match": IntSum({"Stim/S110": 0, "Stim/S111": 1}),
+        "DT_match": IntSum({"Stim/S160": 0, "Stim/S161": 1}),
+        "TLoc": IntSum({f"Stim/S{121 + i}": i + 1 for i in range(12)}),
+        "TOr": IntSum({f"Stim/S{141 + i}": i + 1 for i in range(8)}),
+        "DOr": IntSum({f"Stim/S{151 + i}": i + 1 for i in range(8)}),
+        "Resp_type": CodeLookup(
+            {"yes": "Stim/S192", "no": "Stim/S193", "none": "Stim/S194"}
+        ),
+        "Correct": IntSum({"Stim/S189": 1}),
+    },
+    rt_defs=[
+        RTMeasure(
+            "RT",
+            start=["Stim/S110", "Stim/S111"],
+            end=["Stim/S190", "Stim/S189", "Stim/S191"],
+        )
+    ],
+)
+
+
+def get_metadata(evt, ids, f=None):
+    return parse_metadata(spec, evt, ids, csv_path=f)
+>>>>>>> Stashed changes
